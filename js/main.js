17606ad92ab9e3c51e2a069de945bdb2633d02ad
Mansor Almossa <malmossa@gmail.com>
2021-10-31 18:49:56 -0700

    Completed

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
-function HelloWorld() {
-  alert("Hello World!");
+// All Selectors
+const userInput = document.getElementById("userString");
+const btnSubmit = document.getElementById("btnSubmit");
+const alertBox = document.getElementById("alert");
+const message = document.getElementById("message");
+
+// Event Listener
+btnSubmit.addEventListener("click", reverseAstring);
+
+function reverseAstring() {
+  const input = userInput.value;
+  let stringInReverse = "";
+
+  for (let i = input.length - 1; i >= 0; i--) {
+    stringInReverse += input[i];
+  }
+
+  message.innerHTML = stringInReverse;
+  alertBox.classList.remove("invisible");
 }
